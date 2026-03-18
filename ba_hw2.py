b0fedf3e06ef7bacd63010ec6d8793a120116911
cs25m213-gif <cs25m213@iittp.ac.in>
2026-03-18 17:50:52 +0530

    added greedy algorithm

--- ba_hw2.py
+++ ba_hw2.py
@@ -45,13 +45,28 @@ def greedy(bandit, T):
     Pulls each arm once to initialise, then always picks argmax(mean)."""
     K = bandit.K
     #Initialize algorithm dependent variable here
+    arm_count = np.zeros(K) #for counting number of times arm is pulled
+    prob_per_arm = np.zeros(K) #for calculating how many times each arm obtained the reward and updating the probabilities accordingly
     rewards, regrets = [], []
     cumulative_regret = 0
 
     # Initialise: pull each arm once
     for arm in range(K):
-
+        reward = bandit.pull(arm)
+        arm_count[arm] = 1
+        prob_per_arm[arm] = reward
+        rewards.append(reward)
+        regret = bandit.best_mean - bandit.means[arm] # regret = optimal mean - obtained mean
+        cumulative_regret += regret
+        regrets.append(cumulative_regret) # we are storing the cummulative regret and time increases along horizon
     for t in range(K, T):
+        arm = np.argmax(prob_per_arm)
+        prob_per_arm[arm] = (prob_per_arm[arm]*arm_count[arm]+reward)/(arm_count[arm]+1) # updating arms probability
+        arm_count[arm] += 1
+        rewards.append(reward)
+        regret = bandit.best_mean-bandit.means[arm]
+        cumulative_regret += regret
+        regrets.append(cumulative_regret)
 
     return np.array(rewards), np.array(regrets)
 
@@ -108,7 +123,7 @@ def run_experiment(means, T, n_runs):
         "Greedy":            greedy,
         "Eps-Greedy(0.1)":   lambda b, T: epsilon_greedy(b, T, epsilon=0.1),
         "UCB1":              ucb1,
-        "KL-UCB":            kl_ucb,
+        # "KL-UCB":            kl_ucb,
         "Thompson Sampling": thompson_sampling,
     }
     results = {name: [] for name in algorithms}
